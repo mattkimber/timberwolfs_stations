523a773c8b0902936881ae78ef8596f23c9dee24
Matt Kimber <github@mattkimber.org.uk>
2020-08-31 22:03:46 +0100

    test loading sprites

--- build.sh
+++ build.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Render sprites
+# Create directories
+mkdir -p intermediate
+mkdir -p intermediate/platforms_bare
+mkdir -p intermediate/platforms
 
-# Platforms
-../gorender/renderobject.exe -m files/manifest_platform.json -p -8 -s 1 -r -u voxels/platforms/*.vox
+# Composite objects
+echo "Compositing platform sprites"
+../cargopositor/cargopositor.exe -o intermediate/platforms_bare -v voxels -t positor/platform_objects.json
+
+# Crowds
+echo "Compositing platform sprites (with crowds)"
+../cargopositor/cargopositor.exe -o intermediate/platforms -v intermediate/platforms_bare -t positor/crowds.json
+
+
+# Render sprites
+echo "Rendering platforms"
+../gorender/renderobject.exe -m files/manifest_platform.json -p -8 -s 1,2 -r -u intermediate/platforms/*.vox
+echo ""
 
 # Create NFO
+echo "Creating NFO"
 ../stationer/stationer.exe > test.nfo
 
 # Compile GRF
-../grfcodec/grfcodec.exe -e test.nfo .
+../grfcodec/grfcodec.exe -g 2 -e test.nfo .
