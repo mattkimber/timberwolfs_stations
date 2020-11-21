795f1ee44a7dc7e2a664943c0aeae300e364bedc
Matt Kimber <github@mattkimber.org.uk>
2020-11-21 22:49:29 +0000

    tiled wall shelter

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ mkdir -p intermediate/platforms
 mkdir -p intermediate/bridges
 mkdir -p intermediate/bufferstops
 
+mkdir -p intermediate/tiled_wall_shelter
+mkdir -p intermediate/tiled_wall_shelter_crowds
+
 
 # Basic objects (lamps/shelters)
 # one per platform type
@@ -55,6 +58,13 @@ echo "Compositing ramp sprites (fences)"
 ../cargopositor/cargopositor.exe -o intermediate/platforms -v intermediate/ramps_crowds -t positor/fences_concrete_ramp_1.json
 ../cargopositor/cargopositor.exe -o intermediate/platforms -v intermediate/ramps_crowds -t positor/fences_concrete_ramp_2.json
 
+# Tiled wall shelter
+echo "Compositing tiled wall shelter"
+../cargopositor/cargopositor.exe -o intermediate/tiled_wall_shelter -v intermediate/platforms_bare -t positor/shelter_tiled_wall.json
+../cargopositor/cargopositor.exe -o intermediate/tiled_wall_shelter_crowds -v intermediate/tiled_wall_shelter -t positor/crowds.json
+../cargopositor/cargopositor.exe -o intermediate/platforms -v intermediate/tiled_wall_shelter_crowds -t positor/shelter_tiled_wall_fences.json
+
+
 # Roofed platforms
 echo "Compositing roof supports"
 ../cargopositor/cargopositor.exe -o intermediate/platforms -v intermediate/platforms_crowds -t positor/roof_supports.json
@@ -68,7 +78,7 @@ echo "Compositing buffer stops (fences)"
 
 # Render sprites
 echo "Rendering platforms"
-../gorender/renderobject.exe -m files/manifest_platform.json -p -8 -s 1,2 -r -u intermediate/platforms/*.vox
+ls intermediate/platforms/*.vox | xargs ../gorender/renderobject.exe -m files/manifest_platform.json -p -8 -s 1,2 -r -u 
 echo ""
 
 echo "Rendering buffer stops"
